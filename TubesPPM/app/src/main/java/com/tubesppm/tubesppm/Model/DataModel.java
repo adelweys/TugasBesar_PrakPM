cace621291806e074d0c262f727eb7125b0e227d
Luis Christoryan Silalahi <95632939+luis766hi@users.noreply.github.com>
2023-12-21 11:39:19 +0700

    Update DataModel.java
    done model!

--- TubesPPM/app/src/main/java/com/tubesppm/tubesppm/Model/DataModel.java
+++ TubesPPM/app/src/main/java/com/tubesppm/tubesppm/Model/DataModel.java
@@ -1,1 +1,23 @@
+package com.tubesppm.tubesppm.Model;
+
+public class DataModel {
+        private int id;
+        private String nama, jurusan, judul;
+
+        public int getId() {
+            return id;
+        }
+
+        public String getNama() {
+            return nama;
+        }
+
+        public String getJurusan() {
+            return jurusan;
+        }
+
+        public String getJudul() {
+            return judul;
+        }
+}
 
