1e88fa88d505883d4dd56cad1fca77b7bad7fad6
yohanangl <154459924+yohanangl@users.noreply.github.com>
2023-12-21 11:46:17 +0700

    Update RetroServer.java
    Update RetroServer Edit

--- TubesPPM/app/src/main/java/com/tubesppm/tubesppm/API/RetroServer.java
+++ TubesPPM/app/src/main/java/com/tubesppm/tubesppm/API/RetroServer.java
@@ -1,1 +1,20 @@
+package com.tubesppm.tubesppm.API;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetroServer {
+    private static final String baseURL = "http://192.168.74.39/tubesppm/";
+    private static Retrofit retro;
+
+    public static Retrofit konekRetrofit(){
+        if(retro == null){
+            retro = new Retrofit.Builder()
+                    .baseUrl(baseURL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            }
+        return retro;
+    }
+}
 
